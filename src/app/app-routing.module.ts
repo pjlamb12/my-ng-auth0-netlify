d0dc7f35522b8d905716b8193ec65b6cde2e0b1c
Preston Lamb <preston.j.lamb@gmail.com>
2019-10-07 20:20:53 -0600

    added a basic application with Auth0 authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CallbackComponent } from './callback/callback.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AuthGuard } from './auth.guard';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		component: HomeComponent,
+	},
+	{
+		path: 'callback',
+		component: CallbackComponent,
+	},
+	{
+		path: 'profile',
+		component: ProfileComponent,
+		canActivate: [AuthGuard],
+	},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
